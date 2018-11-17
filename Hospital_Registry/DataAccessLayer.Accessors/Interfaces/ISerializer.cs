3c0a0df9b2ac4bf1580d68264a9ecb73d558d349
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2018-11-17 11:43:59 +0300

    feat: add xml serializer and ISerializer interface

---
+++ Hospital_Registry/DataAccessLayer.Accessors/Interfaces/ISerializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Accessors.Interfaces
+{
+    interface ISerializer<TEntity>
+    {
+        void Serialize(TEntity item, string filePath, System.IO.FileMode fileMode);
+        TEntity Deserialize(string filePath, System.IO.FileMode fileMode);
+    }
+}
