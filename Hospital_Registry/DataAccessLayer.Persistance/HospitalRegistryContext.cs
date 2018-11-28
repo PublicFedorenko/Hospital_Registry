42c1feb84e692b2b53c8be5e3947b5f8dd898eba
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2018-11-28 18:20:57 +0300

    feat: added EF DbContext child class with collections of entities

---
+++ Hospital_Registry/DataAccessLayer.Persistance/HospitalRegistryContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using BusinessLogicLayer.Entities.Entities;
+
+namespace DataAccessLayer.Persistance
+{
+    public class HospitalRegistryContext : DbContext
+    {
+        DbSet<Patient> Patients { get; set; }
+        DbSet<Doctor> Doctors { get; set; }
+    }
+}
