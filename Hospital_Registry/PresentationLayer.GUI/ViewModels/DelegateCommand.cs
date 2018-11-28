04ca7a7aac5f24dbb6bd27ec9fa4f8481e7952c9
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2018-11-28 18:51:55 +0300

    feat(MainWindowViewModel): added DelegateCommand realization for MVVM pattern

---
+++ Hospital_Registry/PresentationLayer.GUI/ViewModels/DelegateCommand.cs
@@ -0,0 +1,49 @@
+﻿using PresentationLayer.GUI.ViewModels.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentationLayer.GUI.ViewModels
+{
+    public class DelegateCommand : IDelegateCommand
+    {
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public DelegateCommand(Action<object> execute)
+        {
+            _execute = execute;
+            _canExecute = AlwaysCanExecute;
+        }
+
+        public DelegateCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        private bool AlwaysCanExecute(object param)
+        {
+            return true;
+        }
+
+        public void Execute(object param)
+        {
+            _execute(param);
+        }
+
+        public bool CanExecute(object param)
+        {
+            return _canExecute(param);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ Hospital_Registry/PresentationLayer.GUI/ViewModels/Interfaces/IDelegateCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PresentationLayer.GUI.ViewModels.Interfaces
+{
+    interface IDelegateCommand : ICommand
+    {
+        void RaiseCanExecuteChanged();
+    }
+}
