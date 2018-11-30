56961a9e1b2b52285dda13471396ee602d465111
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2018-11-30 19:35:14 +0300

    feat(DAL.Persistance): added UnitOfWork interface and implementation

---
+++ Hospital_Registry/DataAccessLayer.Persistance/Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Persistance.Repositories.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IPatientRepository Patients { get; }
+        IDoctorRepository Doctors { get; }
+        int Complete();
+    }
+}
---
+++ Hospital_Registry/DataAccessLayer.Persistance/Repositories/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using DataAccessLayer.Persistance.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Persistance.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly HospitalRegistryContext _context;
+
+        public IPatientRepository Patients { get; private set; }
+        public IDoctorRepository Doctors { get; private set; }
+
+        public UnitOfWork(HospitalRegistryContext context)
+        {
+            _context = context;
+            Patients = new PatientRepository(_context);
+            Doctors = new DoctorRepository(_context);
+        }
+
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
