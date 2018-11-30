45a25726378e12b14017e8ce887bb36fb412ef39
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2018-11-30 13:04:59 +0300

    feat: added services project

---
+++ Hospital_Registry/BusinessLogicLayer.Services/Class1.cs
@@ -0,0 +1,19 @@
+﻿using DataAccessLayer.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Services
+{
+    public class HospitalRegistryService
+    {
+        public HospitalRegistryContext Context { get; }
+
+        public HospitalRegistryService()
+        {
+            Context = new HospitalRegistryContext();
+        }
+    }
+}
