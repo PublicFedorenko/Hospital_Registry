b6adaa10ce6fa118d7d09941ddd8c47eb95f8f4c
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2018-11-30 19:33:43 +0300

    feat(GUI.Persistance): added Patient and Doctor repository interfaces and implementations

---
+++ Hospital_Registry/DataAccessLayer.Persistance/Repositories/DoctorRepository.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLogicLayer.Entities.Entities;
+using DataAccessLayer.Persistance.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Persistance.Repositories
+{
+    public class DoctorRepository : Repository<Doctor>, IDoctorRepository
+    {
+        public DoctorRepository(HospitalRegistryContext context)
+            : base(context)
+        {
+        }
+    }
+}
---
+++ Hospital_Registry/DataAccessLayer.Persistance/Repositories/PatientRepository.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLogicLayer.Entities.Entities;
+using DataAccessLayer.Persistance.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Persistance.Repositories
+{
+    public class PatientRepository : Repository<Patient>, IPatientRepository
+    {
+        public PatientRepository(HospitalRegistryContext context)
+            : base(context)
+        {
+        }
+    }
+}
