4bc144e0b2a4026c9bb3ddeaa4e06819ec35402c
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2018-12-01 19:42:01 +0300

    fix: recreated a project

--- Hospital_Registry/DataAccessLayer.Persistance/Properties/AssemblyInfo.cs
+++ Hospital_Registry/DataAccessLayer.Persistance/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b897e36c-b1b1-48fd-a180-aa930df5d548")]
+[assembly: Guid("a7b17c28-d4bd-4364-9509-e0e246575d0b")]
 
 // Version information for an assembly consists of the following four values:
 //
