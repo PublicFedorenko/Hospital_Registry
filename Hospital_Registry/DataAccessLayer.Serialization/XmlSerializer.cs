36fa703307f85168db2ee19ab9a05231ce0958c0
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2018-12-03 22:07:51 +0300

    feat: added xml serializer implementation

---
+++ Hospital_Registry/DataAccessLayer.Serialization/XmlSerializer.cs
@@ -0,0 +1,37 @@
+﻿using DataAccessLayer.Serialization.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DataAccessLayer.Serialization
+{
+    public class XmlSerializer<T> : ISerializer<T> where T : class
+    {
+        private XmlSerializer xmlSerializer;
+
+        public XmlSerializer()
+        {
+            xmlSerializer = new XmlSerializer(typeof(T));
+        }
+
+        public void Serialize(T item, string filePath, FileMode fileMode)
+        {
+            using (FileStream fs = new FileStream(filePath, fileMode))
+            {
+                xmlSerializer.Serialize(fs, item);
+            }
+        }
+
+        public T Deserialize(string filePath, FileMode fileMode)
+        {
+            using (FileStream fs = new FileStream(filePath, fileMode))
+            {
+                return (T)xmlSerializer.Deserialize(fs);
+            }
+        }
+    }
+}
