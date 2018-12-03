0318e25208ec794a6acc3f716bfe587137eff1a5
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2018-12-03 22:13:56 +0300

    feat: implemented INotifyPropertyChanged interface

--- Hospital_Registry/BusinessLogicLayer.Entities/Entities/Person.cs
+++ Hospital_Registry/BusinessLogicLayer.Entities/Entities/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,21 @@ namespace BusinessLogicLayer.Entities.Entities
     public enum Gender { Male, Female }
 
     [Serializable]
-    public class Person
+    public class Person : INotifyPropertyChanged
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        private DateTime _dateOfBirth;
+        public DateTime DateOfBirth
+        {
+            get => _dateOfBirth;
+            set
+            {
+                _dateOfBirth = value;
+                OnPropertyChanged("Age");
+                OnPropertyChanged("DateOfBirth");
+            }
+        }
         public int Age { get => DateTime.Today.Year - DateOfBirth.Year; }
         public Gender Gender { get; set; }
         public Person() { }
@@ -24,5 +35,14 @@ namespace BusinessLogicLayer.Entities.Entities
             DateOfBirth = dateOfBirth;
             Gender = gender;
         }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
     }
 }
