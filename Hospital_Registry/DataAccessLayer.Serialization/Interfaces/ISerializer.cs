77292e46f7576aad847c5a0168eaaf9529419890
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2018-12-03 22:07:17 +0300

    feat: added xml serializer interface

---
+++ Hospital_Registry/DataAccessLayer.Serialization/Interfaces/ISerializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Serialization.Interfaces
+{
+    public interface ISerializer<T> where T : class
+    {
+        void Serialize(T item, string filePath, FileMode fileMode);
+        T Deserialize(string filePath, FileMode fileMode);
+    }
+}
