b1bba5c42584204097d1c4df3ca05cc878db1c18
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2019-01-04 17:53:17 +0300

    feat: added page for doctors

--- Hospital_Registry/PresentationLayer.GUI/ViewModels/MainWindowViewModel.cs
+++ Hospital_Registry/PresentationLayer.GUI/ViewModels/MainWindowViewModel.cs
@@ -16,8 +16,10 @@ namespace PresentationLayer.GUI.ViewModels
     public class MainWindowViewModel : INotifyPropertyChanged
     {
         private ObservableCollection<Patient> _patients;
+        private ObservableCollection<Doctor> _doctors;
         private ObservableCollection<Visit> _patientAppointments;
         private readonly string _defaultPatientsFilePath;
+        private readonly string _defaultDoctorsFilePath;
 
         #region Commands
         public IDelegateCommand AddPatientCommand { get; protected set; }
@@ -28,6 +30,13 @@ namespace PresentationLayer.GUI.ViewModels
         public IDelegateCommand FindPatients { get; protected set; }
         public IDelegateCommand AddAppointment { get; protected set; }
         public IDelegateCommand RemoveAppointment { get; protected set; }
+
+        public IDelegateCommand AddDoctorCommand { get; protected set; }
+        public IDelegateCommand RemoveDoctorCommand { get; protected set; }
+        public IDelegateCommand EditDoctorCommand { get; protected set; }
+        public IDelegateCommand CompleteEditDoctorCommand { get; protected set; }
+        public IDelegateCommand RefreshDoctor { get; protected set; }
+        public IDelegateCommand FindDoctors { get; protected set; }
         #endregion
 
         #region INotifyPropertyChanged
@@ -42,7 +51,9 @@ namespace PresentationLayer.GUI.ViewModels
         public MainWindowViewModel()
         {
             _defaultPatientsFilePath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Patients.xml"));
+            _defaultDoctorsFilePath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Doctors.xml"));
             _patients = Load<Patient>(_defaultPatientsFilePath);
+            _doctors = Load<Doctor>(_defaultDoctorsFilePath);
 
             #region InitializeCommands
             AddPatientCommand = new DelegateCommand(ExecuteAddPatient);
@@ -53,6 +64,13 @@ namespace PresentationLayer.GUI.ViewModels
             FindPatients = new DelegateCommand(ExecuteFindPatients, CanExecuteFindPatients);
             AddAppointment = new DelegateCommand(ExecuteAddAppointment);
             RemoveAppointment = new DelegateCommand(ExecuteRemoveAppointment, CanExecuteRemoveAppointment);
+
+            AddDoctorCommand = new DelegateCommand(ExecuteAddDoctor);
+            RemoveDoctorCommand = new DelegateCommand(ExecuteRemoveDoctor, CanExecuteRemoveDoctor);
+            EditDoctorCommand = new DelegateCommand(ExecuteEditDoctor, CanExecuteEditDoctor);
+            CompleteEditDoctorCommand = new DelegateCommand(ExecuteComleteEditDoctor, CanExecuteComleteEditDoctor);
+            RefreshDoctor = new DelegateCommand(ExecuteRefreshDoctor, CanExecuteRefreshDoctor);
+            FindDoctors = new DelegateCommand(ExecuteFindDoctors, CanExecuteFindDoctors);
             #endregion
         }
 
@@ -72,10 +90,33 @@ namespace PresentationLayer.GUI.ViewModels
         {
             get
             {
-                _patientAppointments = new ObservableCollection<Visit>(_patients[PatientsListBoxSelectedIndex].SheduledVisits);
+                try
+                {
+                    _patientAppointments = new ObservableCollection<Visit>(_patients[PatientsListBoxSelectedIndex].SheduledVisits);
+
+                }
+                catch (Exception ex) { }
                 return _patientAppointments;
             }
         }
+
+        #region SaveEntities
+        private void Save<TEntity>(ObservableCollection<TEntity> entities, string filePath)
+        {
+            HospitalRegistryService<ObservableCollection<TEntity>> hospitalRegistryService =
+                new HospitalRegistryService<ObservableCollection<TEntity>>();
+
+            hospitalRegistryService.Write(entities, filePath, FileMode.Truncate);
+        }
+
+        private ObservableCollection<TEntity> Load<TEntity>(string filePath)
+        {
+            HospitalRegistryService<ObservableCollection<TEntity>> hospitalRegistryService =
+                new HospitalRegistryService<ObservableCollection<TEntity>>();
+
+            return hospitalRegistryService.Read(filePath, FileMode.Open);
+        }
+        #endregion
 
         #region UtilityProperties
         private int _patientsListSelectedIndex;
@@ -121,21 +162,6 @@ namespace PresentationLayer.GUI.ViewModels
         }
         #endregion
 
-        private void Save<TEntity>(ObservableCollection<TEntity> entities, string filePath)
-        {
-            HospitalRegistryService<ObservableCollection<TEntity>> hospitalRegistryService =
-                new HospitalRegistryService<ObservableCollection<TEntity>>();
-
-            hospitalRegistryService.Write(entities, filePath, FileMode.Truncate);
-        }
-
-        private ObservableCollection<TEntity> Load<TEntity>(string filePath)
-        {
-            HospitalRegistryService<ObservableCollection<TEntity>> hospitalRegistryService =
-                new HospitalRegistryService<ObservableCollection<TEntity>>();
-
-            return hospitalRegistryService.Read(filePath, FileMode.Open);
-        }
 
         #region AddPatient
         private void ExecuteAddPatient(object param)
@@ -195,6 +221,7 @@ namespace PresentationLayer.GUI.ViewModels
             IsPatientReadOnly = true;
             Save(_patients, _defaultPatientsFilePath);
             OnPropertyChanged("IsPatientReadOnly");
+            OnPropertyChanged("PatientsList");
         }
         #endregion
 
@@ -271,6 +298,143 @@ namespace PresentationLayer.GUI.ViewModels
             OnPropertyChanged("PatientsAppointmentsListBoxSelectedIndex");
             OnPropertyChanged("PatientAppointments");
             OnPropertyChanged("PatientsList");
+        }
+        #endregion
+
+
+
+        public ObservableCollection<Doctor> DoctorsList
+        {
+            get
+            {
+                return _doctors;
+            }
+            set
+            {
+                _doctors = value;
+            }
+        }
+
+        #region UtilityProperties
+        private int _doctorsListSelectedIndex;
+        public int DoctorsListBoxSelectedIndex
+        {
+            get => _doctorsListSelectedIndex;
+            set
+            {
+                _doctorsListSelectedIndex = value;
+                RemoveDoctorCommand.RaiseCanExecuteChanged();
+                OnPropertyChanged("IsDoctorSelected");
+            }
+        }
+
+        private bool _isDoctorReadonly = true;
+        public bool IsDoctorReadOnly
+        {
+            get => _isDoctorReadonly;
+            set
+            {
+                _isDoctorReadonly = value;
+                EditDoctorCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public bool IsDoctorSelected
+        {
+            get => DoctorsListBoxSelectedIndex != -1;
+        }
+        #endregion
+
+        #region AddDoctor
+        private void ExecuteAddDoctor(object param)
+        {
+            Doctor doctor = new Doctor()
+            {
+                FirstName = "Empty",
+                LastName = "Doctor",
+                DateOfBirth = new DateTime(2000, 1, 1),
+                Gender = Gender.Male,
+                Adress = new Adress()
+            };
+
+            _doctors.Add(doctor);
+            Save(_doctors, _defaultDoctorsFilePath);
+            OnPropertyChanged("DoctorsList");
+        }
+        #endregion
+
+        #region RemoveDoctor
+        public bool CanExecuteRemoveDoctor(object param)
+        {
+            return DoctorsListBoxSelectedIndex != -1;
+        }
+
+        public void ExecuteRemoveDoctor(object param)
+        {
+            Doctor selectedDoctor = _doctors[DoctorsListBoxSelectedIndex];
+            _doctors.Remove(selectedDoctor);
+            Save(_doctors, _defaultDoctorsFilePath);
+            RemoveDoctorCommand.RaiseCanExecuteChanged();
+            OnPropertyChanged("DoctorsList");
+        }
+        #endregion
+
+        #region EditDoctor
+        public bool CanExecuteEditDoctor(object param)
+        {
+            return DoctorsListBoxSelectedIndex != -1;
+        }
+
+        public void ExecuteEditDoctor(object param)
+        {
+            IsDoctorReadOnly = false;
+            OnPropertyChanged("IsDoctorReadOnly");
+        }
+        #endregion
+
+        #region CompleteEditDoctor
+        public bool CanExecuteComleteEditDoctor(object param)
+        {
+            return DoctorsListBoxSelectedIndex != -1;
+        }
+
+        public void ExecuteComleteEditDoctor(object param)
+        {
+            IsDoctorReadOnly = true;
+            Save(_doctors, _defaultDoctorsFilePath);
+            OnPropertyChanged("IsDoctorReadOnly");
+        }
+        #endregion
+
+        #region RefreshDoctor
+        public bool CanExecuteRefreshDoctor(object param)
+        {
+            return DoctorsListBoxSelectedIndex != -1;
+        }
+
+        public void ExecuteRefreshDoctor(object param)
+        {
+            _doctors = Load<Doctor>(_defaultDoctorsFilePath);
+            DoctorsListBoxSelectedIndex = 0;
+            OnPropertyChanged("DoctorsList");
+        }
+        #endregion
+
+        #region FindDoctors
+        public bool CanExecuteFindDoctors(object param)
+        {
+            return DoctorsList.Count > 0;
+        }
+
+        public void ExecuteFindDoctors(object param)
+        {
+            _doctors = new ObservableCollection<Doctor>(Load<Doctor>(_defaultDoctorsFilePath).Where(
+                p =>
+                {
+                    return p.FirstName.ToLower().Contains(((string)param).ToLower()) ||
+                           p.LastName.ToLower().Contains(((string)param).ToLower());
+                }));
+            OnPropertyChanged("DoctorsList");
         }
         #endregion
     }
